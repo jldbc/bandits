86f3e72f943eae5b5aa8db2804450afb3d0e4b2a
jldbc <jamesledoux@Jamess-MacBook-Pro.local>
2019-12-24 17:40:49 -0800

    add processing functions for movielens 1m dataset

--- bandits/scripts/create_movielens_dataset.py
+++ bandits/scripts/create_movielens_dataset.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np 
 
-def read_data():
+def read_data_20m():
 	print('reading movielens 20m data')
 	ratings = pd.read_csv('../data/ml-20m/ratings.csv')
 	movies = pd.read_csv('../data/ml-20m/movies.csv')
@@ -12,7 +12,43 @@ def read_data():
 	logs = ratings.join(movies, on='movieId', how='left', rsuffix='_movie')
 	return logs
 
-def preprocess_movie_data(logs, min_number_of_reviews=20000):
+def read_data_1m():
+	print('reading movielens 1m data')
+	ratings = pd.read_csv('../../data/ml-1m/ratings.dat', 
+		sep='::',
+		names=[
+			'user_id',
+			'movie_id',
+			'rating',
+			'ts'
+		])
+	movies = pd.read_csv('../../data/ml-1m/movies.dat', 
+		sep='::',
+		names=[
+			'movie_id',
+			'title',
+			'genres'
+		])
+	users = pd.read_csv('../../data/ml-1m/users.dat', 
+		sep='::', 
+		names = [
+			'user_id',
+			'gender',
+			'age',
+			'occupation',
+			'zip'
+		])
+	logs = ratings.join(movies, on='movie_id', how='left', rsuffix='_movie')
+	logs = logs.join(users, on='user_id', how='left', rsuffix='_movie')
+	return logs
+
+def process_title():
+	pass
+
+def process_genres():
+	pass
+
+def preprocess_movie_data_20m(logs, min_number_of_reviews=20000):
 	print('preparing ratings log')
 	# remove ratings of movies with < N ratings. too few ratings will cause the recsys to get stuck in offline evaluation
 	movies_to_keep = pd.DataFrame(logs.movieId.value_counts())\
@@ -26,9 +62,28 @@ def preprocess_movie_data(logs, min_number_of_reviews=20000):
 	logs['liked'] = logs['rating'].apply(lambda x: 1 if x >= 4.5 else 0)
 	return logs
 
-def get_ratings(min_number_of_reviews=20000):
-	logs = read_data()
-	logs = preprocess_movie_data(logs, min_number_of_reviews=20000)
+def preprocess_movie_data_1m(logs, min_number_of_reviews=1000):
+	print('preparing ratings log')
+	# remove ratings of movies with < N ratings. too few ratings will cause the recsys to get stuck in offline evaluation
+	movies_to_keep = pd.DataFrame(logs.movie_id.value_counts())\
+		.loc[pd.DataFrame(logs.movie_id.value_counts())['movie_id']>=min_number_of_reviews].index
+	logs = logs.loc[logs['movie_id'].isin(movies_to_keep)]
+	# shuffle rows to deibas order of user ids
+	logs = logs.sample(frac=1)
+	# create a 't' column to represent time steps for the bandit to simulate a live learning scenario
+	logs['t'] = np.arange(len(logs))
+	logs.index = logs['t']
+	logs['liked'] = logs['rating'].apply(lambda x: 1 if x >= 4.5 else 0)
+	return logs
+
+def get_ratings_20m(min_number_of_reviews=20000):
+	logs = read_data_20m()
+	logs = preprocess_movie_data_20m(logs, min_number_of_reviews=20000)
+	return logs
+
+def get_ratings_1m(min_number_of_reviews=1000):
+	logs = read_data_1m()
+	logs = preprocess_movie_data_1m(logs, min_number_of_reviews=1000)
 	return logs
 
 def __init__():
